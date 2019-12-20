6c3d993d002a645b5a534862e48cc5c714a5ad67
rahul kumar <rahulkumar4@virtualemployee.com>
2019-12-20 11:55:21 +0530

    done

--- app/src/main/java/com/app/mylibertadriver/activities/HelpActivity.java
+++ app/src/main/java/com/app/mylibertadriver/activities/HelpActivity.java
@@ -1,18 +1,26 @@
 package com.app.mylibertadriver.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.app.mylibertadriver.R;
+import com.app.mylibertadriver.databinding.ActivityHelpBinding;
 
 public class HelpActivity extends AppCompatActivity {
+    private ActivityHelpBinding binder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_help);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        binder = DataBindingUtil.setContentView(this, R.layout.activity_help);
 
     }
 }
